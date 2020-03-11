44ac61cefa92251247a8067b59da347bd3b1b578
Denny Koch <denny.koch@scenid.com>
2020-03-11 20:02:46 +0100

    cleanup firebird query

--- packages/cubejs-schema-compiler/adapter/FirebirdQuery.js
+++ packages/cubejs-schema-compiler/adapter/FirebirdQuery.js
@@ -35,55 +35,8 @@ class FirebirdQuery extends BaseQuery {
     return ` ROWS ${limit}`
   }
 
-  newFilter(filter) {
-    return new FirebirdFilter(this, filter);
-  }
-
-  convertTz(field) {
-    return `CONVERT_TZ(${field}, @@session.time_zone, '${moment().tz(this.timezone).format('Z')}')`;
-  }
-
-  timeStampCast(value) {
-    return `TIMESTAMP(convert_tz(${value}, '+00:00', @@session.time_zone))`;
-  }
-
-  inDbTimeZone(date) {
-    return this.inIntegrationTimeZone(date).clone().utc().format(moment.HTML5_FMT.DATETIME_LOCAL_MS);
-  }
-
-  dateTimeCast(value) {
-    return `TIMESTAMP(${value})`;
-  }
-
-  subtractInterval(date, interval) {
-    return `DATE_SUB(${date}, INTERVAL ${interval})`;
-  }
-
-  addInterval(date, interval) {
-    return `DATE_ADD(${date}, INTERVAL ${interval})`;
-  }
-
-  timeGroupedColumn(granularity, dimension) {
-    return GRANULARITY_TO_INTERVAL[granularity](dimension);
-  }
-
   escapeColumnName(name) {
     return `${name}`;
-  }
-
-  seriesSql(timeDimension) {
-    const values = timeDimension.timeSeries().map(
-      ([from, to]) => `select '${from}' f, '${to}' t`
-    ).join(' UNION ALL ');
-    return `SELECT TIMESTAMP(dates.f) date_from, TIMESTAMP(dates.t) date_to FROM (${values}) AS dates`;
-  }
-
-  concatStringsSql(strings) {
-    return `CONCAT(${strings.join(", ")})`;
-  }
-
-  unixTimestampSql() {
-    return `UNIX_TIMESTAMP()`;
   }
 }
 
